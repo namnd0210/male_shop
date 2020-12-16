85432df9f2d2425356957499fdf0f54ae21ad5ff
ducnam <namnd0210>
2020-12-16 10:24:56 +0700

    refactor file

---
+++ header.php
@@ -0,0 +1,68 @@
+<div class="header__top">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 col-md-7">
+                <div class="header__top__left">
+                    <p>
+                        Miễn phí vận chuyển, hoàn trả trong 30 ngày hoặc đảm bảo hoàn
+                        lại tiền.
+                    </p>
+                </div>
+            </div>
+            <div class="col-lg-6 col-md-5">
+                <div class="header__top__right">
+                    <div class="header__top__links">
+                        <a href="#">Đăng nhập</a>
+                        <a href="#">FAQs</a>
+                    </div>
+                    <div class="header__top__hover">
+                        <span>Usd <i class="arrow_carrot-down"></i></span>
+                        <ul>
+                            <li>USD</li>
+                            <li>EUR</li>
+                            <li>USD</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="container">
+    <div class="row">
+        <div class="col-lg-3 col-md-3">
+            <div class="header__logo">
+                <a href="./index.php"><img src="img/logo.png" alt="" /></a>
+            </div>
+        </div>
+        <div class="col-lg-6 col-md-6">
+            <nav class="header__menu mobile-menu">
+                <ul>
+                    <li class="active"><a href="./index.php">Trang chủ </a></li>
+                    <li><a href="./#">Shop</a></li>
+                    <li>
+                        <a href="#">Trang</a>
+                        <ul class="dropdown">
+                            <li><a href="./#">Thông tin về chúng tôi</a></li>
+                            <li><a href="./#">Chi tiết cửa hàng</a></li>
+                            <li><a href="./#">Giỏ hàng</a></li>
+                            <li><a href="./#">Check Out</a></li>
+                            <li><a href="./#">Blog</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="./#">Liên hệ</a></li>
+                    <li><a href="./order.php">Đơn hàng</a></li>
+                </ul>
+            </nav>
+        </div>
+        <div class="col-lg-3 col-md-3">
+            <div class="header__nav__option">
+                <a href="#" class="search-switch"><img src="img/icon/search.png" alt="" /></a>
+                <a href="#"><img src="img/icon/heart.png" alt="" /></a>
+                <a href="#"><img src="img/icon/cart.png" alt="" /> <span>0</span></a>
+                <div class="price">$0.00</div>
+            </div>
+        </div>
+    </div>
+    <div class="canvas__open"><i class="fa fa-bars"></i></div>
+</div>
