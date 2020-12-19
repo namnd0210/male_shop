b530e0e53e34053fc292b426818346e00de0e636
ducnam <namnd0210>
2020-12-19 16:17:11 +0700

    split footer

--- order-details.php
+++ order-details.php
@@ -310,98 +310,25 @@ if (isset($_SESSION['user_id'])) {
                 </div>
             </div>
         </div>
+    </div>
+    </div>
 
-        <!-- Footer Section Begin -->
-        <footer class="footer">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-3 col-md-6 col-sm-6">
-                        <div class="footer__about">
-                            <div class="footer__logo">
-                                <a href="#"><img src="img/footer-logo.png" alt="" /></a>
-                            </div>
-                            <p>
-                                Khách hàng là trọng tâm của mô hình kinh doanh độc đáo của chúng
-                                tôi, bao gồm cả thiết kế.
-                            </p>
-                            <a href="#"><img src="img/payment.png" alt="" /></a>
-                        </div>
-                    </div>
-                    <div class="col-lg-2 offset-lg-1 col-md-3 col-sm-6">
-                        <div class="footer__widget">
-                            <h6>Shopping</h6>
-                            <ul>
-                                <li><a href="#">Cửa hàng quần áo</a></li>
-                                <li><a href="#">Giày xu hướng</a></li>
-                                <li><a href="#">Phụ kiện</a></li>
-                                <li><a href="#">Giảm giá</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="col-lg-2 col-md-3 col-sm-6">
-                        <div class="footer__widget">
-                            <h6>Shopping</h6>
-                            <ul>
-                                <li><a href="#">Liên hệ chúng tôi</a></li>
-                                <li><a href="#">Phương thức thanh toán</a></li>
-                                <li><a href="#">Vận chuyển</a></li>
-                                <li><a href="#">Hoàn trả</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="col-lg-3 offset-lg-1 col-md-6 col-sm-6">
-                        <div class="footer__widget">
-                            <h6>NewLetter</h6>
-                            <div class="footer__newslatter">
-                                <p>
-                                    Hãy là người đầu tiên biết về hàng mới xuất hiện, xem sách,
-                                    bán hàng và quảng cáo!
-                                </p>
-                                <form action="#">
-                                    <input type="text" placeholder="Your email" />
-                                    <button type="submit">
-                                        <span class="icon_mail_alt"></span>
-                                    </button>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-lg-12 text-center">
-                        <div class="footer__copyright__text">
-                            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-                            <p>
-                                Copyright ©
-                                <script>
-                                document.write(new Date().getFullYear());
-                                </script>
-                                2020 All rights reserved | This template is made with
-                                <i class="fa fa-heart-o" aria-hidden="true"></i> by
-                                <a href="https://colorlib.com" target="_blank">Colorlib</a>
-                            </p>
-                            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </footer>
 
-        <!-- Footer Section End -->
+    <?php include('footer.php') ?>
 
-        <!-- Js Plugins -->
-        <script type="module" src="js/order.js"></script>
+    <!-- Js Plugins -->
+    <script type="module" src="js/order.js"></script>
 
-        <script src="js/jquery-3.3.1.min.js"></script>
-        <script src="js/bootstrap.min.js"></script>
-        <script src="js/jquery.nice-select.min.js"></script>
-        <script src="js/jquery.nicescroll.min.js"></script>
-        <script src="js/jquery.magnific-popup.min.js"></script>
-        <script src="js/jquery.countdown.min.js"></script>
-        <script src="js/jquery.slicknav.js"></script>
-        <script src="js/mixitup.min.js"></script>
-        <script src="js/owl.carousel.min.js"></script>
-        <script src="js/main.js"></script>
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.nice-select.min.js"></script>
+    <script src="js/jquery.nicescroll.min.js"></script>
+    <script src="js/jquery.magnific-popup.min.js"></script>
+    <script src="js/jquery.countdown.min.js"></script>
+    <script src="js/jquery.slicknav.js"></script>
+    <script src="js/mixitup.min.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/main.js"></script>
 </body>
 
 </html>
